05b4d5db20f35700fedf270b1828339435edf045
Anand_Mahadev <anandmahadev07@gmail.com>
2024-09-05 19:30:31 +0530

    good changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.get('/weatherReport')
 def tell_the_whether():
-    city = input('Enter city name: ')
+    city = 'coimbatore'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
