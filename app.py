f980b8d52a78facc6f0a2ecedf6c49f98775f573
Anand_Mahadev <anandmahadev07@gmail.com>
2024-09-05 19:30:08 +0530

    new change

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #app.py
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
-
+api_key = '196107f35a838a648a4fc4dbbde0868e'
 
 @app.get('/')
 def hello_world():
@@ -10,7 +11,21 @@ def hello_world():
 
 @app.get('/weatherReport')
 def tell_the_whether():
-    return 'veyyil vandha mazha daa...'
+    city = input('Enter city name: ')
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        temp = data['main']['temp']
+        desc = data['weather'][0]['description']
+        print(f'Temperature: {temp} K')
+        print(f'Description: {desc}')
+        return f'temperature:{temp} and description:{desc}'
+    else:
+        print('Error fetching weather data')
+        return 'error'
+    
+
 
 
 if __name__ == '__main__':
